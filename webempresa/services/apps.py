982d1af1e99d6e946b3cb9d9f41990e93a3bfaf3
aimanjr77tech <f1online7@gmail.com>
2025-11-14 12:08:35 +0100

    actualizacion1

---
+++ webempresa/services/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class ServicesConfig(AppConfig):
+    name = 'services'
+    verbose_name = 'Gestor de servicios'
+    default_auto_field = 'django.db.models.BigAutoField'
+
